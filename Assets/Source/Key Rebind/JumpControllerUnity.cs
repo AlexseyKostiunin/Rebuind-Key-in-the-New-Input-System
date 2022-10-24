4e27c8c2a8be56b61d6b9b96f20f74c0dc1e3ef7
AlexseyKostiunin <85487222+AlexseyKostiunin@users.noreply.github.com>
2022-10-24 13:37:20 +0300

    !F rebind through plugin Unity

---
+++ Assets/Source/Key Rebind/JumpControllerUnity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JumpControllerUnity : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput _playerInput;
+    
+    private RebindJumping gameActions;
+
+    private Rigidbody rb;
+
+    private void OnEnable()
+    {
+        gameActions = InputManager.inputActions;
+
+        _playerInput.actions["Interact"].started += DoJump;
+
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    private void DoJump(InputAction.CallbackContext obj)
+    {
+        rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+    }
+}
