f16d15830bee4bf6f0c710091e6d7778dff9bc40
AlexseyKostiunin <85487222+AlexseyKostiunin@users.noreply.github.com>
2022-11-21 15:15:36 +0300

    !R

--- Assets/Source/ExitGame.cs
+++ Assets/Source/ExitGame.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ExitGame : MonoBehaviour
 {
     public void Exit()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
